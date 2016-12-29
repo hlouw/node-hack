10f54b0ffa386a2f5331942967f3363387ebf862
Hentie Louw <hentie.louw@gmail.com>
2016-12-29 18:37:23 +0000

    Typescripty changes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import * as restify from 'restify';
+import { RequestHandler, createServer } from 'restify';
 
-const respond: restify.RequestHandler = (req, res, next) => {
+const respond: RequestHandler = (req, res, next) => {
   res.send('hello there, ' + req.params.name);
   next();
 }
 
-const server = restify.createServer();
+const server = createServer();
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 
-server.listen(8080, function() {
+server.listen(8080, () => {
   console.log('%s listening at %s', server.name, server.url);
 });
