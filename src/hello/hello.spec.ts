7ce1c1cf28d68c9d3da8257ffd85c252e11fc187
Ed Conolly <ed.conolly@ovoenergy.com>
2016-12-30 11:56:00 +0000

    Remove 2As comments

--- src/hello/hello.spec.ts
+++ src/hello/hello.spec.ts
@@ -3,35 +3,20 @@ import {generateHello} from "./hello";
 
 describe("generateHello()", () => {
   it("should error when a name contains a number", () => {
-    // Arrange.
     const name = "Freddie12";
-
-    // Act.
     const result = generateHello(name);
-
-    // Assert.
     assert.instanceOf(result, Error);
   });
 
   it("should error when a name contains a special character", () => {
-    // Arrange.
     const name = "Freddie£";
-
-    // Act.
     const result = generateHello(name);
-
-    // Assert.
     assert.instanceOf(result, Error);
-  })
+  });
 
   it("should generate a welcome message if the name is alphabetic", () => {
-      // Arrange.
       const name = "Freddie";
-
-      // Act.
       const result = generateHello(name);
-
-      // Assert.
       assert.equal(result.message, "hello Freddie");
-  })
+  });
 });
